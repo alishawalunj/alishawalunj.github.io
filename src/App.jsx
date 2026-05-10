6e3e4df2aff35be3d9a9828b8d72bdad7cf01f81
ALISHA WALUNJ <walunjalisha@gmail.com>
2026-05-10 18:56:23 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold">Alisha Walunj</h1>
+        <p className="mt-4 text-xl text-gray-400">
+          Software Engineer
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
